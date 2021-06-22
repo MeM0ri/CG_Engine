4e99b4946853ab79cc7ef1aa9bd644bbe814e702
MeM0ri <angarchanin69@gmail.com>
2021-06-23 02:09:58 +0300

    Add Icon and some changes with Exception handler.

--- Window.cpp
+++ Window.cpp
@@ -1,4 +1,5 @@
 #include "Window.h"
+#include "resource.h"
 #include <sstream>
 
 /*--------------Get in touch with Window Class--------------*/
@@ -15,12 +16,12 @@ Window::WindowClass::WindowClass() noexcept
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = GetInstance();
-	wc.hIcon = nullptr;
+	wc.hIcon = static_cast<HICON>(LoadImage(hInst,MAKEINTRESOURCE(IDB_PNG1),IMAGE_ICON, 393, 369,0));
 	wc.hCursor = nullptr;
 	wc.hbrBackground = nullptr;
 	wc.lpszMenuName = nullptr;
 	wc.lpszClassName = GetName();
-	wc.hIconSm = nullptr;
+	wc.hIconSm = static_cast<HICON>(LoadImage(hInst, MAKEINTRESOURCE(IDB_PNG1), IMAGE_ICON, 393, 369, 0));
 	RegisterClassEx(&wc);
 }
 
